724d57ab998504edeb048f190560226f8f69be31
Roman Zhak <zhak55@ya.ru>
2015-02-07 19:39:25 +0300

    Update array.js

--- array.js
+++ array.js
@@ -48,24 +48,3 @@ function array( arr ) {
     
     return Object.create(o).constructor(); 
 };
-
-// test 
- var arr = array(["js", "css", "html"]);
-
-       arr 
-        .on("push", handler )
-        .on("unshift", handler )
-        .on("pop", handler )
-        .on("shift", handler );
-
-    arr
-     .pop()
-     .shift()
-     .unshift( "html" )
-     .push( "js" )
-
- function handler( elements, type ) {
-    console.log( type,  elements )
-    // remove handler   
-  }
-
