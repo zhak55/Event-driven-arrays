bcf2918b5e54bf6734d5b244cfcf12ed5511413d
Roman Zhak <zhak55@ya.ru>
2015-02-07 19:37:28 +0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,71 @@
+function array( arr ) {
+    var p = Array.prototype
+      , o = {}
+      , subs = {};
+    
+    // Add one handler to event type
+    //  @param {String} [type] - event type accorting to methods of array 
+    //  @param {Function} [callback]
+    
+    o.on = function( type, callback ) {
+      subs[type] = callback;
+      return this;
+    }
+    
+    // Remove one handler 
+    // @param {String} [type]
+    
+    o.off = function( type ) {
+      if( subs[type] ) delete subs[type];  
+    }
+    
+    o.trigger = function( type, el ) {
+        if( subs[type] ) subs[type].call( this, el, type );
+    };
+    
+    // create some methods to test
+   
+    ["push", "unshift", "shift", "pop"].forEach(function( name ) {
+      o[ name ] = function() {
+        var args = arguments; 
+          p[ name ].apply( this, args );
+          this.trigger( name, args );
+        return this;
+      }
+    });
+    
+    // Return converted object to array;
+    
+    o.native = function() {
+     return p.slice.call(this);   
+    }
+    
+    o.constructor = function() {
+      for( var k in arr ) this[k] = arr[k];
+       this.length = arr.length;
+       return this;
+    }
+    
+    return Object.create(o).constructor(); 
+};
+
+// test 
+ var arr = array(["js", "css", "html"]);
+
+       arr 
+        .on("push", handler )
+        .on("unshift", handler )
+        .on("pop", handler )
+        .on("shift", handler );
+
+    arr
+     .pop()
+     .shift()
+     .unshift( "html" )
+     .push( "js" )
+
+ function handler( elements, type ) {
+    console.log( type,  elements )
+    // remove handler   
+  }
+
